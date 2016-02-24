241db25c81c0c85a36e076fc8a933a4935bad411
Adam Veary <adamjv90@gmail.com>
2016-02-24 09:57:28 -0500

    inital commit

--- cplusplusfun/main.cpp
+++ cplusplusfun/main.cpp
@@ -7,9 +7,39 @@
 //
 
 #include <iostream>
+#include <time.h>
+#include <string>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    // notifications = array of epoch notification times
+    std::string notifications[] = {"1456323204", "1456150404", "1456668804", "1456841604"};
+    
+    // now = current epoch time
+    time_t now;
+    time(&now);
+    
+    int size = sizeof(notifications) / sizeof(std::string);
+    
+    double timediff;
+    struct tm * notificationTimeTm;
+    
+    for(int i = 0; i < size; i++) {
+        time_t notificationTime = (time_t) notifications[i].c_str();
+        notificationTimeTm = localtime(&notificationTime);
+        
+        timediff = difftime(now, mktime(notificationTimeTm));
+        
+        // this output doesnt look right so something is off here
+        std::cout << std::to_string(timediff) << "\n";
+    }
+    
+    struct tm * nowTm;
+    nowTm = localtime(&now);
+    
+    std::cout << "set hours too: " << nowTm->tm_hour << "\n";
+    std::cout << "set minutes too: " << nowTm->tm_min << "\n";
+    
+    
     return 0;
 }
