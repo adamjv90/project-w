7f5e734850255d37822ce70ebc5537f352457e57
Adam Veary <adamjv90@gmail.com>
2016-02-23 22:22:04 -0500

    Initial Commit

---
+++ cplusplusfun/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cplusplusfun
+//
+//  Created by Adam Veary on 2/23/16.
+//  Copyright © 2016 Adam Veary. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
